c7b13a2f21e30b78ae6e6df55c2be3bc3eac0c3f
Naz2204 <ngvoznyuk@gmail.com>
2025-03-12 19:36:20 +0200

    Added rectangle drawing

--- main.cpp
+++ main.cpp
@@ -17,6 +17,9 @@ unsigned int createShaderProgram();
 
 void prepareDrawTriangle(unsigned int& VAO, unsigned int& VBO);
 
+void prepareDrawSquare(unsigned int& VAO, unsigned int& VBO, unsigned int& EBO);
+
+
 int main() {
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -41,9 +44,11 @@ int main() {
     glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
 
     unsigned int shaderProgram = createShaderProgram();
-    unsigned int VBO;
-    unsigned int VAO;
-    prepareDrawTriangle(VAO, VBO);
+    unsigned int VBO, VAO, EBO;
+
+    //prepareDrawTriangle(VAO, VBO);
+
+    prepareDrawSquare(VAO, VBO, EBO);
 
     while(!glfwWindowShouldClose(window)){
         processInput(window);
@@ -53,7 +58,9 @@ int main() {
 
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        //glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        //glBindVertexArray(0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
@@ -61,6 +68,7 @@ int main() {
 
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
     glDeleteProgram(shaderProgram);
     glfwTerminate();
     return 0;
@@ -162,4 +170,34 @@ void prepareDrawTriangle(unsigned int& VAO, unsigned int& VBO) {
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
+}
+
+void prepareDrawSquare(unsigned int& VAO, unsigned int& VBO,  unsigned int& EBO) {
+    float vertices[] = {
+        0.5f,  0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+       -0.5f, -0.5f, 0.0f,
+       -0.5f,  0.5f, 0.0f
+    };
+    unsigned int indices[] = {
+        0, 1, 3,
+        1, 2, 3
+    };
+
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &EBO);
+    glGenBuffers(1, &VBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 }
